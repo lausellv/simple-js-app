15a7152dae737aba53854c0a5f8206b62c025092
lausellv <lausellv@yahoo.com>
2020-09-05 19:28:19 +0200

    opening a new master repository for project

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+let myName = 'Bob';
+myName = "Victor";
+document.write(myName);
+alert(myName);
+
+// let size = 100;
+// let doubleSize = size * 2;
+// document.write(doubleSize);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+// document.write(minSize);
+document.write(doubleSize)
+
+
