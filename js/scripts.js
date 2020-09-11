f6e1f5c7d9d2126be47283f0c955ccdaf8ffa1bc
lausellv <lausellv@yahoo.com>
2020-09-11 19:03:26 +0200

    aaa

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,33 @@ let size = 100;
 let doubleSize = size * 2;
 let minSize = (doubleSize * 2) - (size / 2);
 // document.write(minSize);
-document.write(doubleSize)
+document.write(doubleSize);
 
 let favoriteFood = "Spinach";
 document.write(favoriteFood);
 
-let color ="red"
+let color ="red";
 document.write(color);
+
+let text1 = 'This is a text!';
+let text2 = "This uses double quotes.";
+document.write(text2);
+
+let person = {
+    name: 'Victor',
+    age: 52,
+};
+let annesAge = 27;
+let anne = {
+  name: 'Anne',
+  age: annesAge,
+  child: {
+    name: 'Joe',
+    age: 2
+  }
+};
+anne.name = 'Victor';
+console.log(anne);
+
 
 
