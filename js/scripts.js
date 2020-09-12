0aac0deb47f63a51fe351c9ccb46e22eadc13537
lausellv <lausellv@yahoo.com>
2020-09-12 17:05:32 +0200

    1.2 creating an array of objects Pokemon list version 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,7 @@
 //I started doing this but then I realized I can include the empty objects in the array and modify them afterward.  
 // the exercise calls for an empty array so I created one that's empty bc the objects haven't been defined.
 
-// let pokemonDescription = {
-//   name: {},
-//   type: {},
-//   height: {}
-// };
+
 
 // IN the end I opted out for the following formula. Seems much easier. 
 
@@ -28,8 +24,40 @@ pokemonList[0].height = '0.7m'
 pokemonList[1].height = '1m'
 pokemonList[2].height = '1.7m'
 
+//version 2
+// Another possible way could be to create three empty objects (one for each pokemon)
 
+let pokemon1 = {
+   name: {},
+   type: {},
+   height: {}
+ };
 
+ let pokemon2 = {
+  name: {},
+  type: {},
+  height: {}
+};
 
+let pokemon3 = {
+  name: {},
+  type: {},
+  height: {}
+};
 
+//in oder to create an array of empty objects
+let pokemonListVersB = [pokemon1, pokemon2, pokemon3];
 
+// now I went ahead and entered the info
+
+pokemonListVersB[0].name = 'Charmeleon'
+pokemonListVersB[0].type = 'fire'
+pokemonListVersB[0].height = '1.1m'
+
+pokemonListVersB[1].name = 'Butterfree'
+pokemonListVersB[1].type = 'bug', 'flying'
+pokemonListVersB[1].height = '1.1m'
+
+pokemonListVersB[0].name = 'Rattata'
+pokemonListVersB[0].type = 'normal'
+pokemonListVersB[0].height = '0.3m'
