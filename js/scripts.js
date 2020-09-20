6b9ffb64339c5458819ecbafa69f43c9ac276722
lausellv <lausellv@yahoo.com>
2020-09-20 12:39:10 +0200

    don't know how to style the JS

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@
 let pokemonList = [
   { name: {}}, { type: {}}, {height:{}}, 
   { name: {}}, { type: {}}, {height:{}},
-  { name: {}}, { type: {}}, {height:{}}
+  { name: {}}, { type: {}}, {height:{}},
 ];
 
 // Now I have to add names, types and height to these pokemons  (searched for them on https://pokedex.org/.)
@@ -24,40 +24,69 @@ pokemonList[0].height = '0.7m'
 pokemonList[1].height = '1m'
 pokemonList[2].height = '1.7m'
 
-//version 2
-// Another possible way could be to create three empty objects (one for each pokemon)
+// //version 2
+// // Another possible way could be to create three empty objects (one for each pokemon)
 
-let pokemon1 = {
-   name: {},
-   type: {},
-   height: {}
- };
+// let pokemon1 = {
+//    name: {},
+//    type: {},
+//    height: {}
+//  };
 
- let pokemon2 = {
-  name: {},
-  type: {},
-  height: {}
-};
+//  let pokemon2 = {
+//   name: {},
+//   type: {},
+//   height: {}
+// };
 
-let pokemon3 = {
-  name: {},
-  type: {},
-  height: {}
-};
+// let pokemon3 = {
+//   name: {},
+//   type: {},
+//   height: {}
+// };
 
-//in oder to create an array of empty objects
-let pokemonListVersB = [pokemon1, pokemon2, pokemon3];
+// //in order to create an array of empty objects
+// let pokemonListVersB = [pokemon1, pokemon2, pokemon3];
 
-// now I went ahead and entered the info
+// // now I went ahead and entered the info
 
-pokemonListVersB[0].name = 'Charmeleon'
-pokemonListVersB[0].type = 'fire'
-pokemonListVersB[0].height = '1.1m'
+// pokemonListVersB[0].name = 'Charmeleon'
+// pokemonListVersB[0].type = 'fire'
+// pokemonListVersB[0].height = '1.1m'
 
-pokemonListVersB[1].name = 'Butterfree'
-pokemonListVersB[1].type = 'bug', 'flying'
-pokemonListVersB[1].height = '1.1m'
+// pokemonListVersB[1].name = 'Butterfree'
+// pokemonListVersB[1].type = 'bug', 'flying'
+// pokemonListVersB[1].height = '1.1m'
 
-pokemonListVersB[0].name = 'Rattata'
-pokemonListVersB[0].type = 'normal'
-pokemonListVersB[0].height = '0.3m'
+// pokemonListVersB[0].name = 'Rattata'
+// pokemonListVersB[0].type = 'normal'
+// pokemonListVersB[0].height = '0.3m'
+
+
+// var firstNumber = 3;
+// var secondNumber = 7;
+// var total = Number(firstNumber)+Number(secondNumber);
+// alert(total);
+
+// var firstNumber = prompt('Enter first number');
+// var secondNumber = prompt('Enter second number');
+// var total = Number (firstNumber)+ Number(secondNumber);
+// alert(total);
+
+
+// JavaScript Basics 1.3 part 2
+
+//first try
+// for (var i=0; i<pokemonList.length; i++ ){
+//   alert(pokemonList[i].name + ' ' + '('+ 'height:' + height + ')');
+
+// }
+// second try.  changed to i<3
+for (var i=0; i<3; i++ ){
+  
+  console.log(pokemonList[i].name + ' ' + ' (' + 'type:' + pokemonList[i].type + ') '
+   + '(' + 'height:' + pokemonList[i].height + ')' );
+   document.write('\\ ' + pokemonList[i].name + ' ' + ' (' + 'type:' + pokemonList[i].type + ') '
+   + '(' + 'height:' + pokemonList[i].height + ')  ');
+
+}
